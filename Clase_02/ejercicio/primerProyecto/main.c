21a7cbd29cb69de55a96219b1266b63414acb5b3
alejandrohuk <36174338+alejandrohuk@users.noreply.github.com>
2018-03-21 11:01:32 -0300

    Add files via upload

---
+++ Clase_02/ejercicio/primerProyecto/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+ int main(){
+    int i,suma=0;
+
+    for(i=1;i<=100;i++)
+    {
+    suma=suma+i;
+
+
+    }
+    printf("%i",suma);
+    return 0;
+
+
+ }
+
